0c6ac884278e93c6a06e84e69b80ec62f3ef687a
pedrofalcao10 <falcao.comp@gmail.com>
2023-06-03 11:38:50 -0300

    doc added

--- src/br/ufc/stock/exception/NegativeAmountException.java
+++ src/br/ufc/stock/exception/NegativeAmountException.java
@@ -1,8 +1,19 @@
+/**
+ * pedrofalcao10
+ * The NegativeAmountException class represents an exception that is thrown
+ * when a negative amount is provided where it is not permitted.
+ */
 package br.ufc.stock.exception;
 
-public class NegativeAmountException extends Exception{
+public class NegativeAmountException extends Exception {
     private int amount;
 
+    /**
+     * pedrofalcao10
+     * Constructs a new NegativeAmountException with the specified amount.
+     *
+     * @param amount The negative amount that caused the exception.
+     */
     public NegativeAmountException(int amount) {
         super("Negative amount: " + amount + " isn't permitted.");
         this.amount = amount;
--- src/br/ufc/stock/manager/StockManager.java
+++ src/br/ufc/stock/manager/StockManager.java
@@ -1,3 +1,7 @@
+/**
+ * The StockManager class manages a collection of stocks.
+ * It allows registering and retrieving stocks based on different criteria.
+ */
 package br.ufc.stock.manager;
 
 import br.ufc.stock.Item;
@@ -11,10 +15,22 @@ import java.util.Vector;
 public class StockManager {
     private Vector<Stock> stocksVector;
 
+    /**
+     * pedrofalcao10
+     * Constructs a new StockManager object.
+     * Initializes the internal collection of stocks.
+     */
     public StockManager() {
         stocksVector = new Vector<>();
     }
 
+    /**
+     * pedrofalcao10
+     * Registers a stock by directly adding it to the collection of stocks.
+     *
+     * @param stock The Stock object to be registered.
+     * @throws StockAlreadyExistsException if a stock with the same content already exists in the collection.
+     */
     public void registerByStock(Stock stock) throws StockAlreadyExistsException {
         if(stocksVector.contains(stock)){
             throw new StockAlreadyExistsException(stock);
@@ -22,6 +38,14 @@ public class StockManager {
         stocksVector.add(stock);
     }
 
+    /**
+     * pedrofalcao10
+     * Registers a stock by item type.
+     * Checks if a stock with the same item type already exists before adding it to the collection.
+     *
+     * @param stock The Stock object to be registered.
+     * @throws StockAlreadyExistsException if a stock with the same item type already exists in the collection.
+     */
     public void registerByItem(Stock stock) throws StockAlreadyExistsException {
         Optional<Stock> existingStock = get(stock.getItemType());
 
@@ -32,6 +56,13 @@ public class StockManager {
         stocksVector.add(stock);
     }
 
+    /**
+     * pedrofalcao10
+     * Retrieves a stock that contains the specified item.
+     *
+     * @param item The Item object to search for in the stocks.
+     * @return An Optional containing the found Stock, or an empty Optional if no stock contains the item.
+     */
     public Optional<Stock> get(Item item){
         for (Stock stock : stocksVector) {
             if (stock.containsItem(item)) {
@@ -41,3 +72,4 @@ public class StockManager {
         return Optional.empty();
     }
 }
+
