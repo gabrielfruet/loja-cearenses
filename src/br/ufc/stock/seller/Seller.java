6d66f35d6b518d6ea8e79ca7fdef25d721cd50db
Gabriel Fruet <gabrielfruet538@gmail.com>
2023-06-03 12:43:42 -0300

    Seller doc

--- src/br/ufc/stock/seller/Seller.java
+++ src/br/ufc/stock/seller/Seller.java
@@ -6,8 +6,34 @@ import br.ufc.stock.seller.exception.SellerNegativeAmountException;
 
 import java.math.BigDecimal;
 
+/**
+ * Represents a seller.
+ *
+ * @author Gabriel Fruet
+ */
 public interface Seller {
+    /**
+     * Sells a specified amount of items.
+     *
+     * @param amount the amount of items to sell
+     * @return a Sale object representing the sale
+     * @throws SellerNegativeAmountException if the amount is negative
+     */
     Sale sell(int amount) throws SellerNegativeAmountException;
+
+    /**
+     * Calculates the price for a specified amount of items.
+     *
+     * @param amount the amount of items
+     * @return the price for the specified amount of items
+     * @throws SellerNegativeAmountException if the amount is negative
+     */
     BigDecimal price(int amount) throws SellerNegativeAmountException;
+
+    /**
+     * Retrieves the item type associated with the seller.
+     *
+     * @return the item type
+     */
     Item getItemType();
 }
