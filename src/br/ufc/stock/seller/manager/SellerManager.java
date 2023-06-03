eebda88c32a38287d3328120926223887dd24a3c
Gabriel Fruet <gabrielfruet538@gmail.com>
2023-06-03 12:37:59 -0300

    SellerManager doc

--- src/br/ufc/stock/seller/manager/SellerManager.java
+++ src/br/ufc/stock/seller/manager/SellerManager.java
@@ -8,40 +8,66 @@ import java.util.Optional;
 import java.util.Vector;
 import java.util.stream.Collectors;
 
+/**
+ * Manages a collection of sellers and provides operations to register sellers, retrieve sellers by item type name,
+ * and retrieve all products associated with the registered sellers.
+ *
+ * @author Gabriel Fruet
+ */
 public class SellerManager {
-    private Vector<Seller> sellers;
+    private final Vector<Seller> sellers;
 
-    public SellerManager(){
+    /**
+     * Constructs a new instance of SellerManager.
+     */
+    public SellerManager() {
         sellers = new Vector<Seller>();
     }
 
+    /**
+     * Registers a new seller.
+     *
+     * @param seller the seller to register
+     * @throws ExistentSellerException if a seller already exists in the manager
+     */
     public void register(Seller seller) throws ExistentSellerException {
         boolean exists = false;
-        for(Seller s : this.sellers){
-            if(seller.equals(seller)){
+        for (Seller s : this.sellers) {
+            if (seller.equals(s)) {
                 exists = true;
                 break;
             }
         }
 
-        if(exists){
+        if (exists) {
             throw new ExistentSellerException(seller);
-        }else{
+        } else {
             this.sellers.add(seller);
         }
     }
 
-    public Optional<Seller> get(String name){
-        for(Seller s : sellers){
-            if(s.getItemType().getName().equals(name)){
+    /**
+     * Retrieves a Seller object by the name of the associated item type.
+     *
+     * @param name the name of the item type
+     * @return an Optional containing the Seller object if found, or an empty Optional if not found
+     */
+    public Optional<Seller> get(String name) {
+        for (Seller s : sellers) {
+            if (s.getItemType().getName().equals(name)) {
                 return Optional.of(s);
             }
         }
         return Optional.empty();
     }
 
-    public Vector<Item> getProducts(){
-        return new Vector(
+    /**
+     * Retrieves all products associated with the registered sellers.
+     *
+     * @return a Vector containing all products associated with the registered sellers
+     */
+    public Vector<Item> getProducts() {
+        return new Vector<>(
                 this.sellers
                         .stream()
                         .map(Seller::getItemType)
