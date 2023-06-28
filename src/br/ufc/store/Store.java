7bc54212652d03169839dd70fdbbebc52430b17e
Gabriel Fruet <gabrielfruet538@gmail.com>
2023-06-28 16:24:22 -0300

    mais metodos

--- src/br/ufc/store/Store.java
+++ src/br/ufc/store/Store.java
@@ -1,26 +1,74 @@
 package br.ufc.store;
 
-import br.ufc.stock.manager.StockManager;
-import br.ufc.stock.seller.manager.SellerManager;
+import br.ufc.stock.Item;
+import br.ufc.stock.Request;
+import br.ufc.stock.Stock;
+import br.ufc.stock.sale.Sale;
 import br.ufc.user.VendorManager;
 import br.ufc.user.VendorUser;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Store implements Serializable {
     private BigDecimal cash;
-    private SellerManager sellerManager;
-    private StockManager stockManager;
     private VendorManager vendorManager;
-
     private VendorUser activeUser;
 
-    public void debit(BigDecimal value){
+    public void debit(BigDecimal value) {
+    }
+
+    public void credit(BigDecimal value) {
 
     }
 
-    public void credit(BigDecimal value){
+    public void login(String username, String password) {
 
     }
+
+    public void register(String username, String password) {
+
+    }
+
+    public VendorUser getActiveUser() {
+        return activeUser;
+    }
+
+    public BigDecimal getCash() {
+        return null;
+    }
+
+    public List<Sale> getLastTransactions() {
+        return null;
+    }
+
+    public void createItem(String name, String descriptor) {
+
+    }
+
+    public void updateItem(String descriptor) {
+
+    }
+
+    public List<Item> getItems() {
+        return null;
+    }
+
+    public void createStock(Item itemType, int amount, BigDecimal buyPrice, StoreRequester requester) {
+
+    }
+
+    public List<Stock> getStocks() {
+        return null;
+    }
+
+    public void createSeller(Stock stock, BigDecimal price) {
+
+    }
+
+    public List<Item> getSellers() {
+        return null;
+    }
+
 }
