4e05b4d4f74ec74d4ec61515fa23e2e590c26468
Gabriel Fruet <gabrielfruet538@gmail.com>
2023-06-28 15:59:39 -0300

    Credit now dont need request

--- src/br/ufc/store/StoreRequester.java
+++ src/br/ufc/store/StoreRequester.java
@@ -3,19 +3,13 @@ package br.ufc.store;
 import br.ufc.stock.request.Request;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 public class StoreRequester implements Serializable {
     private Store store;
 
-    public void credit(Request request){
-        try{
-            store.credit(request.getValue());
-            request.conclude();
-        }
-        catch(Exception e){
-            //Fazer algo com a exceção;
-            request.decline();
-        }
+    public void credit(BigDecimal value){
+        store.credit(value);
     }
 
     public void debit(Request request){
