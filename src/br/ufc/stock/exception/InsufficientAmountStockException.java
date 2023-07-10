ef522cf10a551b542cd33294a6aeb6ca4f0bf4e8
victorpmatiass <victorpm2010@gmail.com>
2023-07-09 22:47:58 -0300

    correcao do trabalho

--- src/br/ufc/stock/exception/InsufficientAmountStockException.java
+++ src/br/ufc/stock/exception/InsufficientAmountStockException.java
@@ -7,13 +7,6 @@ package br.ufc.stock.exception;
 
 public class InsufficientAmountStockException extends Exception {
     private int amount;
-
-    /**
-     * pedrofalcao10
-     * Constructs a new InsufficientAmountStockException with the specified amount.
-     *
-     * @param amount The amount of items that caused the exception.
-     */
     public InsufficientAmountStockException(int amount) {
         super("Insufficient Amount in Stock: " + amount);
         this.amount = amount;
--- src/br/ufc/stock/seller/Seller.java
+++ src/br/ufc/stock/seller/Seller.java
@@ -9,35 +9,11 @@ import br.ufc.stock.seller.exception.SellerNegativeAmountException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-/**
- * Represents a seller.
- *
- * @author Gabriel Fruet
- */
+
 public interface Seller {
-    /**
-     * Sells a specified amount of items.
-     *
-     * @param amount the amount of items to sell
-     * @return a Sale object representing the sale
-     * @throws SellerNegativeAmountException if the amount is negative
-     */
+
     Sale sell(int amount) throws SellerNegativeAmountException, NegativeAmountException, InsufficientAmountStockException;
-
-    /**
-     * Calculates the price for a specified amount of items.
-     *
-     * @param amount the amount of items
-     * @return the price for the specified amount of items
-     * @throws SellerNegativeAmountException if the amount is negative
-     */
     BigDecimal price(int amount) throws SellerNegativeAmountException;
-
-    /**
-     * Retrieves the item type associated with the seller.
-     *
-     * @return the item type
-     */
     Item getItemType();
 
 
